5a1da0ed77d245eceb8b713bd09a9092b2f4f95b
Mirko Salaris <salarismirko@gmail.com>
2019-11-04 16:11:27 +0100

    compute and compare Fred and Int
    For cases: m=[5,10,20], N=[30,60,120], patterns: random, decreasing, increasing
    Base modules used:
        little_module = "battle_creekhs_1"
        middle_module = "Valleceppi_P0"
        big_module = "scuola_3"
    
    Problems to be fixed in next commit:
    - distance between portals has to be computed with Manhattan distances
    - the scale of the graphs is wrong (obtained scaling xml to meters directly,
        but the scaling factor was computed from images to meters)

--- src/graphextractor.py
+++ src/graphextractor.py
@@ -1,4 +1,4 @@
-__all__ = ['get_graph', 'graph_to_distance_matrix']
+__all__ = ['get_graph']
 
 import numpy as np
 from bs4 import BeautifulSoup
@@ -6,25 +6,25 @@ from bs4 import BeautifulSoup
 from graph import Graph
 
 
-def find_centroid(space):
-    x = float(space.centroid.point["x"])
-    y = float(space.centroid.point["y"])
+def find_centroid(space, scaling_factor):
+    x = float(space.centroid.point["x"]) * scaling_factor
+    y = float(space.centroid.point["y"]) * scaling_factor
 
     return [x, y]
 
 
-def find_portals(space, soup):
+def find_portals(space, soup, scaling_factor):
     portals = []  # it will contain tuples (x, y)
 
     for portal in space.find_all("portal"):
         segment_id = portal.linesegment.string
         segments = soup.find_all("linesegment", id=segment_id)
-        if len(segments) > 0:  # ... if there's no data associated we can't do nothing about it
+        if len(segments) > 0:  # ... otherwise if there's no data associated we can't do nothing about it
             xs = []
             ys = []
             for s in segments:
-                xs.append(float(s.point["x"]))
-                ys.append(float(s.point["y"]))
+                xs.append(float(s.point["x"])*scaling_factor)
+                ys.append(float(s.point["y"])*scaling_factor)
 
             x = sum(xs) / len(xs)
             y = sum(ys) / len(ys)
@@ -46,8 +46,13 @@ def connect_portals(graph, portals_ids):
                 graph.add_edge(por1_id, por2_id)
 
 
-def get_graph(file):
-    """file: either a filename string or an open file"""
+def get_graph(file, scaling_factor):
+    """ Given an xml file of an environment calculate and return its graph representation
+
+    :param file: either a filename string or an open file
+    :param scaling_factor: the scale factor of the environment representation w.r.t. the desired measurement unit
+    :return: a Graph, and a tuple containing the list of centroids and the list of portals
+    """
 
     opened = False  # keep track of whether we opened the file (the function input was a filename) or not
     try:
@@ -64,10 +69,10 @@ def get_graph(file):
         portals_list = []
 
         for space in soup.find_all("space", id=True):  # id=True to filter only spaces with an id (see poli.xml)
-            centroid = find_centroid(space)
+            centroid = find_centroid(space, scaling_factor)
             centroids_list.append(centroid)
 
-            portals = find_portals(space, soup)
+            portals = find_portals(space, soup, scaling_factor)
             portals_list.append(portals)
 
             centroid_id = graph.add_node(centroid)
